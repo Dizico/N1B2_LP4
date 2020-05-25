3b1541876f8923e122f07ca3b1381e6a09edb81a
dizico <rodrigo.maestro98@gmail.com>
2020-05-25 00:39:07 -0300

    session_storage_por_Adriano_Martins_082160002_e_Rodrigo_Maestro_082160006

--- index.controller.js
+++ index.controller.js
@@ -6,16 +6,27 @@
         .controller('Home.IndexController', Controller);
 
         function Controller(UserService) {
-        var vm = this;
-        vm.user = null;
-        
-        function initUser() {
-            // get current user data in the API
-            UserService.GetUserId().then(function (userId) {
-                UserService.GetCurrent(userId).then(function (user) {
-                        vm.user = user;
-                    });
-            });
+            var vm = this;
+            vm.user = null;
+            
+            let usingSessionStorage = JSON.parse(localStorage.getItem("user"));
+            console.log(usingSessionStorage)
+             
+            if (!usingSessionStorage){
+                initUser();
+            }
+            else{
+            vm.user = usingSessionStorage
+            }
+            
+            function initUser() {
+                UserService.GetUserId().then(function (userId) {
+                    UserService.GetCurrent(userId).then(function (user) {
+                            vm.user = user;
+    
+                            sessionStorage.setItem("user", JSON.stringify(user));
+                        });
+                });
+            }
         }
-    }
 })();
