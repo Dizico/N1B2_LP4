ed78fb709d062942aad4a327b1ea1a4fdaffcdfe
dizico <rodrigo.maestro98@gmail.com>
2020-05-25 00:10:35 -0300

    commit_criacao

---
+++ index.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Home.IndexController', Controller);
+
+        function Controller(UserService) {
+        var vm = this;
+        vm.user = null;
+        
+        function initUser() {
+            // get current user data in the API
+            UserService.GetUserId().then(function (userId) {
+                UserService.GetCurrent(userId).then(function (user) {
+                        vm.user = user;
+                    });
+            });
+        }
+    }
+})();
